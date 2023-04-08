498d2bfecff962130c8d25162a4e26b7b68ba4bc
Marcin Jahn <marcin.jahn@esky.com>
2023-04-08 21:47:13 +0200

    Improve container styling

--- src/astro.config.mjs
+++ src/astro.config.mjs
@@ -4,15 +4,25 @@ import remarkMermaid from 'astro-diagram/remark-mermaid';
 import { astroCodeSnippets, codeSnippetAutoImport } from './integrations/astro-code-snippets';
 import { asideAutoImport, astroAsides } from './integrations/astro-asides';
 import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://marcinjahn.com',
-  integrations: [AutoImport({
-    imports: [asideAutoImport, codeSnippetAutoImport]
-  }), astroAsides(), astroCodeSnippets(), mdx(), sitemap()],
+  integrations: [
+    AutoImport({imports: [asideAutoImport, codeSnippetAutoImport]}), 
+    astroAsides(), 
+    astroCodeSnippets(), 
+    mdx(), 
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   markdown: {
     remarkPlugins: [remarkMermaid],
     syntaxHighlight: 'shiki',
