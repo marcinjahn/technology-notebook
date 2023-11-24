f96b575d008ebb799c45541242579e1c95a7bb5e
Marcin Jahn <marcin.jahn@esky.com>
2023-11-24 08:50:16 +0100

    Fix typos + attempt at trailing slashes fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://marcinjahn.com',
+  trailingSlash: 'never',
   integrations: [
     AutoImport({imports: [asideAutoImport, codeSnippetAutoImport]}), 
     astroAsides(), 
