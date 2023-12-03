d72a974141c04ceb099e164fe179f8eaf10e8fa0
Marcin Jahn <marcin.jahn@esky.com>
2023-12-03 12:26:59 +0100

    Update styling of code blocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,39 @@
 import { defineConfig } from 'astro/config';
 import AutoImport from 'astro-auto-import';
 import remarkMermaid from 'astro-diagram/remark-mermaid';
-import { astroCodeSnippets, codeSnippetAutoImport } from './integrations/astro-code-snippets';
 import { asideAutoImport, astroAsides } from './integrations/astro-asides';
 import mdx from "@astrojs/mdx";
-import partytown from "@astrojs/partytown";
-
+// import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
+import expressiveCode from "astro-expressive-code";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://marcinjahn.com',
   trailingSlash: 'never',
-  integrations: [
-    AutoImport({imports: [asideAutoImport, codeSnippetAutoImport]}), 
-    astroAsides(), 
-    astroCodeSnippets(), 
-    mdx(), 
-    sitemap(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-  ],
+  integrations: [AutoImport({
+    imports: [asideAutoImport]
+  }), sitemap(), astroAsides(), expressiveCode({
+
+    themes: ['one-dark-pro'],
+    styleOverrides: {
+      frames: {
+        frameBoxShadowCssValue: 'rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px',
+        tooltipSuccessBackground: '#035166'
+      }
+    }
+  }), 
+  mdx(),
+  partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }),
+],
   markdown: {
     remarkPlugins: [remarkMermaid],
-    syntaxHighlight: 'shiki',
+    syntaxHighlight: 'shiki'
   }
 });
