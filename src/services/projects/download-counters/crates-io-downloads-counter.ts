33cf12ad00af14954940243d1c9fd8464cb19cf7
Marcin Jahn <10273406+marcinjahn@users.noreply.github.com>
2024-09-15 08:31:32 +0200

    Add header to crates.io API call to fix 403

--- src/services/projects/download-counters/crates-io-downloads-counter.ts
+++ src/services/projects/download-counters/crates-io-downloads-counter.ts
@@ -1,18 +1,30 @@
-import type { DownloadsCounterResult, DownloadsCounter } from "./downloads-counter";
+import type {
+  DownloadsCounterResult,
+  DownloadsCounter,
+} from "./downloads-counter";
 
 export class CratesIoDownloadsCounter implements DownloadsCounter {
   constructor(private packageName: string) {}
 
   async getCount(): Promise<DownloadsCounterResult> {
-    const response = await fetch(`https://crates.io/api/v1/crates/${this.packageName}`);
+    const response = await fetch(
+      `https://crates.io/api/v1/crates/${this.packageName}`,
+      {
+        headers: {
+          "User-Agent": "marcinjahn.com build",
+        },
+      },
+    );
 
-    if(!response.ok) {
-      throw new Error(`crates.io downloads counter failed for ${this.packageName}. Error code: ${response.status}`);
+    if (!response.ok) {
+      throw new Error(
+        `crates.io downloads counter failed for ${this.packageName}. Error code: ${response.status}`,
+      );
     }
 
-    return await response.json().then(json => ({
+    return await response.json().then((json) => ({
       downloads: json.crate.downloads as number,
-      source: 'crates.io'
+      source: "crates.io",
     }));
   }
 }
