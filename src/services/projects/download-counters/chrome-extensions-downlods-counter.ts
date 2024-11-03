3033c92f7ae7cc47f7abaade502bf1aef93166ba
Marcin Jahn <10273406+marcinjahn@users.noreply.github.com>
2024-11-03 16:09:26 +0100

    Fix Chrome stats scraping by adding some User-Agent header
    Without that, the site returns 403.
    Also applies the same fix for Edge stats scraping, although
    that one does not experience the issue (yet).

--- src/services/projects/download-counters/chrome-extensions-downlods-counter.ts
+++ src/services/projects/download-counters/chrome-extensions-downlods-counter.ts
@@ -1,30 +1,51 @@
-import * as cheerio from 'cheerio';
-import type { DownloadsCounterResult, DownloadsCounter } from "./downloads-counter";
+import * as cheerio from "cheerio";
+import type {
+  DownloadsCounterResult,
+  DownloadsCounter,
+} from "./downloads-counter";
 
 export class ChromeExtensionsDownloadsCounter implements DownloadsCounter {
   constructor(private extensionId: string) {}
 
   async getCount(): Promise<DownloadsCounterResult> {
-    const response = await fetch(`https://chrome-stats.com/d/${this.extensionId}`);
+    const response = await fetch(
+      `https://chrome-stats.com/d/${this.extensionId}`,
+      {
+        headers: {
+          "User-Agent":
+            "Mozilla/5.0 (X11; Linux x86_64; rv:132.0) Gecko/20100101 Firefox/132.0",
+        },
+      },
+    );
 
     if (!response.ok) {
-      throw new Error(`Google Chrome downloads counter failed for ${this.extensionId}. Error code: ${response.status}`);
+      throw new Error(
+        `Google Chrome downloads counter failed for ${this.extensionId}. Error code: ${response.status}`,
+      );
     }
 
-    return await response.text().then(text => {
+    return await response.text().then((text) => {
       const $ = cheerio.load(text);
 
-      const extractedText = $(`.left-panel a[href="/d/${this.extensionId}/trends"]`).first().text().trim().replaceAll(',', '');
+      const extractedText = $(
+        `.left-panel a[href="/d/${this.extensionId}/trends"]`,
+      )
+        .first()
+        .text()
+        .trim()
+        .replaceAll(",", "");
 
       if (!extractedText) {
-        throw new Error('Could not find the HTML node representing active users on Chrome Web Store');
+        throw new Error(
+          "Could not find the HTML node representing active users on Chrome Web Store",
+        );
       }
 
       const downloadsNumber = parseInt(extractedText);
 
       return {
         downloads: downloadsNumber,
-        source: "Weekly active users on Chrome Web Store"
+        source: "Weekly active users on Chrome Web Store",
       };
     });
   }
--- src/services/projects/download-counters/edge-extensions-downloads-counter.ts
+++ src/services/projects/download-counters/edge-extensions-downloads-counter.ts
@@ -1,30 +1,51 @@
-import * as cheerio from 'cheerio';
-import type { DownloadsCounterResult, DownloadsCounter } from "./downloads-counter";
+import * as cheerio from "cheerio";
+import type {
+  DownloadsCounterResult,
+  DownloadsCounter,
+} from "./downloads-counter";
 
 export class EdgeExtensionsDownloadsCounter implements DownloadsCounter {
   constructor(private extensionId: string) {}
 
   async getCount(): Promise<DownloadsCounterResult> {
-    const response = await fetch(`https://edge-stats.com/d/${this.extensionId}`);
+    const response = await fetch(
+      `https://edge-stats.com/d/${this.extensionId}`,
+      {
+        headers: {
+          "User-Agent":
+            "Mozilla/5.0 (X11; Linux x86_64; rv:132.0) Gecko/20100101 Firefox/132.0",
+        },
+      },
+    );
 
     if (!response.ok) {
-      throw new Error(`Edge Addons downloads counter failed for ${this.extensionId}. Error code: ${response.status}`);
+      throw new Error(
+        `Edge Addons downloads counter failed for ${this.extensionId}. Error code: ${response.status}`,
+      );
     }
 
-    return await response.text().then(text => {
+    return await response.text().then((text) => {
       const $ = cheerio.load(text);
 
-      const extractedText = $(`.left-panel a[href="/d/${this.extensionId}/trends"]`).first().text().trim().replaceAll(',', '');
+      const extractedText = $(
+        `.left-panel a[href="/d/${this.extensionId}/trends"]`,
+      )
+        .first()
+        .text()
+        .trim()
+        .replaceAll(",", "");
 
       if (!extractedText) {
-        throw new Error('Could not find the HTML node representing active users on Edge Addons');
+        throw new Error(
+          "Could not find the HTML node representing active users on Edge Addons",
+        );
       }
 
       const downloadsNumber = parseInt(extractedText);
 
       return {
         downloads: downloadsNumber,
-        source: "Weekly active users on Edge Addons"
+        source: "Weekly active users on Edge Addons",
       };
     });
   }
