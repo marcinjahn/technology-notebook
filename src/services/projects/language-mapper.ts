9311dfba74f8be7c8707f3639348539673201636
Marcin Jahn <10273406+marcinjahn@users.noreply.github.com>
2025-07-12 17:24:20 +0200

    Add lua to project languages

--- src/services/projects/language-mapper.ts
+++ src/services/projects/language-mapper.ts
@@ -4,99 +4,109 @@ export interface ProgrammingLanguage {
   color: string;
 }
 
-export function mapProgrammingLanguage(language: string | null): ProgrammingLanguage | null {
-  switch(language) {
+export function mapProgrammingLanguage(
+  language: string | null,
+): ProgrammingLanguage | null {
+  switch (language) {
     case "JavaScript":
       return {
         fullName: language,
         shortName: "JS",
-        color: "#F1E05A"
+        color: "#F1E05A",
       };
     case "TypeScript":
       return {
         fullName: language,
         shortName: "TS",
-        color: "#3178C6"
+        color: "#3178C6",
       };
     case "C#":
       return {
         fullName: language,
         shortName: "C#",
-        color: "#178600"
+        color: "#178600",
       };
     case "Rust":
       return {
         fullName: language,
         shortName: "Rust",
-        color: "#DEA584"
+        color: "#DEA584",
       };
     case "Shell":
       return {
         fullName: language,
         shortName: "sh",
-        color: "#89E051"
+        color: "#89E051",
       };
     case "HTML":
       return {
         fullName: language,
         shortName: "html",
-        color: "#E34C26"
+        color: "#E34C26",
       };
     case "Astro":
       return {
         fullName: language,
         shortName: "astro",
-        color: "#FF5A03"
+        color: "#FF5A03",
       };
     case "C++":
       return {
         fullName: language,
         shortName: "C++",
-        color: "#F34B7D"
+        color: "#F34B7D",
       };
     case "Vue":
       return {
         fullName: language,
         shortName: "Vue",
-        color: "#41B883"
+        color: "#41B883",
       };
     case "Rich Text Format":
       return {
         fullName: language,
         shortName: "RTF",
-        color: "#CCCCCC"
+        color: "#CCCCCC",
       };
     case "CSS":
       return {
         fullName: language,
         shortName: "css",
-        color: "#563D7C"
+        color: "#563D7C",
       };
     case "Go":
       return {
         fullName: language,
         shortName: "Go",
-        color: "#00ADD8"
+        color: "#00ADD8",
       };
     case "Vala":
       return {
         fullName: language,
         shortName: "Vala",
-        color: "#A56DE2"
+        color: "#A56DE2",
       };
     case "Java":
       return {
         fullName: language,
         shortName: "Java",
-        color: "#B07219"
+        color: "#B07219",
       };
     case "Arduino":
       return {
         fullName: language,
         shortName: "ard",
-        color: "#EDEDED"
+        color: "#EDEDED",
+      };
+
+    case "Lua":
+      return {
+        fullName: language,
+        shortName: "lua",
+        color: "#00007F",
       };
     default:
       return null;
   }
 }
+
